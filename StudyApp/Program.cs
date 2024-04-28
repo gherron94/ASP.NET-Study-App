1104fb3cc03bd31cb1c713853a9f82cf84bc2b2f
gherron94 <140562713+gherron94@users.noreply.github.com>
2024-04-28 15:25:01 +0100

    created databse tables

--- StudyApp/Program.cs
+++ StudyApp/Program.cs
@@ -21,6 +21,17 @@ builder.Services.AddRazorPages();
 builder.Services.AddDbContext<StudyContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("StudyContext")));
 
 var app = builder.Build();
+CreateDbIfNotExists(app);
+
+  static void CreateDbIfNotExists(IHost host)
+      {
+        using (var scope = host.Services.CreateScope())
+        {
+          var services = scope.ServiceProvider;
+          var context = services.GetRequiredService<StudyContext>();
+          context.Database.EnsureCreated();
+        }
+      }
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
