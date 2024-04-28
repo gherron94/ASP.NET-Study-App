d660144c73e7aad24c075c885779e2e1fb10b994
gherron94 <140562713+gherron94@users.noreply.github.com>
2024-04-28 13:28:23 +0100

    added study context in data folder

---
+++ StudyApp/Data/StudyContext.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using StudyApp.Models;
+
+namespace StudyApp.Data
+{
+  public class StudyContext : DbContext
+  {
+
+    public DbSet<DayTest> Days
+    {get; set;}
+    public DbSet<StudyData> StudyData
+    {get; set;}
+
+    public StudyContext(DbContextOptions<StudyContext> options): base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<DayTest>().ToTable("DayTest");
+      modelBuilder.Entity<StudyData>().ToTable("StudyData");
+
+    }
+
+  }
+}
